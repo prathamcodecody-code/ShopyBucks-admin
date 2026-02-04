5143b41baf537819cffb1f74f46c9b6ea47d1219
prathamcodecody-code <pratham.codecody@gmail.com>
2026-02-04 12:02:42 +0530

    Update AdminLayout.tsx

--- components/AdminLayout.tsx
+++ components/AdminLayout.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import Sidebar from "./Sidebar";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
+    <div className="flex bg-amazon-lightGray min-h-screen">
+      {/* SIDEBAR Component */}
       <Sidebar />
-
-      <main className="flex-1 ml-0 md:ml-64 pt-16 md:pt-0 p-6 bg-brandCream min-h-screen">
-        {children}
+         <main className="flex-1 ml-0 md:ml-72 pt-20 md:pt-0 min-h-screen transition-all duration-300">
+        <div className="p-4 md:p-8">
+          {children}
+        </div>
       </main>
     </div>
   );
